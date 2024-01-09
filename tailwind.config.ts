fa7ea058839270fa04e4c58492bdbb7f75cfa6fa
TaymaZ <taymazak1382@gmail.com>
2024-01-09 15:06:32 +0330

    chore: init next js Starter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,38 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        xs: '480px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1380px',
+      },
+      boxShadow: {
+        base: '0px 1px 10px rgba(0, 0, 0, 0.05)',
+      },
+      spacing: {
+        25: '6.25rem',
+        50: '12.5rem',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          lg: '0.625rem',
+        },
+      },
+      colors: {
+        white: '#FEFEFF',
+        black: '#0D0D0D',
+      },
+      fontFamily: {
+        iranyekan: 'IRANYekan',
       },
     },
   },
-  plugins: [],
 }
 export default config
